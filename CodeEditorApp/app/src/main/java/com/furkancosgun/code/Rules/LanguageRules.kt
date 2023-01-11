e3b07bdb6c4825e4197ac0f53930cb6a4ab1181c
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-11 19:45:34 +0300

    Create LanguageRules.kt

---
+++ CodeEditorApp/app/src/main/java/com/furkancosgun/code/Rules/LanguageRules.kt
@@ -0,0 +1,65 @@
+package com.furkancosgun.code.Rules
+
+import com.furkancosgun.code.Model.RuleMatch
+
+
+interface LanguageRules {
+    /**
+     * match on Constants
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchConstants(string: CharSequence): List<RuleMatch>
+
+    /**
+     * match on Comments
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchComments(string: CharSequence): List<RuleMatch>
+
+    /**
+     * match on Annotations
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchAnnotations(string: CharSequence): List<RuleMatch>
+
+    /**
+     * match on Classes
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchClasses(string: CharSequence): List<RuleMatch>
+
+    /**
+     * match on Imports
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchImports(string: CharSequence): List<RuleMatch>
+
+    /**
+     * match on Keywords
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun matchKeywords(string: CharSequence): List<RuleMatch>
+
+    /**
+     * Combine matching rules
+     *
+     * @param string CharSequence sequence of chars to perform rule matching on
+     * @return List<RuleMatch> list of rule matches
+     */
+    fun createHighlighting(string: CharSequence): List<RuleMatch> {
+        return matchKeywords(string) + matchAnnotations(string) + matchClasses(string) +
+                matchConstants(string) + matchComments(string) + matchImports(string)
+    }
+}
