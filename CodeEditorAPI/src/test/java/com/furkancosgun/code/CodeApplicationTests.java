1ba33163cee332ece38b1cc2b2ced3346d867240
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-11 19:45:13 +0300

    others

---
+++ CodeEditorAPI/src/test/java/com/furkancosgun/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkancosgun.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
